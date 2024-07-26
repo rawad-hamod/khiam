038ec41337e05170073f7b9cd4b32b2cb5d29190
rawad-hamod <rawadwhamod@gamil.com>
2024-07-26 12:55:53 +0300

    no thing

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,11 +1,8 @@
 import { Paper, Typography } from "@mui/material";
 
-import SignUp from "./SignUp";
-
 export default function HomePage() {
   return (
     <div>
-      
       <Paper>
         <Typography variant="h2" sx={{marginTop:"400px"}} >
           home page
